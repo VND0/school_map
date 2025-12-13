28fed00278d738cdd0ee195cd9dd5fdfd7f53ea0
user <varlamov577123@gmail.com>
2025-12-13 20:33:42 +0300

    Added routes to get object by its id

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import uvicorn
-
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from fastapi.exceptions import HTTPException
+from fastapi.responses import HTMLResponse, Response
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy import select
 
 import setup_data
 from database.database import create_db_and_tables, get_session
+from database.db_models import Object
 
 app = FastAPI()
-
+sessions = get_session()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
@@ -17,6 +19,34 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
+
+
+@app.get("/object-data")
+async def get_object_data(identifier: int):
+    session = next(sessions)
+    map_object: Object = session.query(select(Object).where(Object.id == identifier)).one_or_none()
+    if not map_object:
+        raise HTTPException(404, "Map object not found")
+
+    return {
+        "title": map_object.title,
+        "description": map_object.description,
+        "url_to_image": f"/object-image/{identifier}"
+    }
+
+
+@app.get("/object-image/identifier")
+async def geet_object_image(identifier: int):
+    session = next(sessions)
+    map_object: Object = session.query(select(Object).where(Object.id == identifier)).one_or_none()
+    if not map_object:
+        raise HTTPException(404, "Map object not found")
+
+    image = map_object.image_path
+    return Response(
+        content=image,
+        media_type="application/octet-stream",
+    )
 
 
 if __name__ == "__main__":
