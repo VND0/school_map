f58b71ad5b077e0af101568c3a8bf145e58cced1
user <varlamov577123@gmail.com>
2025-12-16 19:58:59 +0300

    Small code cleanup and style.css changes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi.exceptions import HTTPException
 from fastapi.responses import HTMLResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import select
 
 import setup_data.script
 from database.database import create_db_and_tables, get_session
@@ -37,7 +36,6 @@ async def get_object_data(identifier: int):
 
 @app.get("/object-image/{identifier}")
 async def get_object_image(identifier: int):
-    print(identifier)
     session = next(sessions)
     map_object: Object = session.query(Object).filter(Object.id == identifier).one_or_none()
     if not map_object:
