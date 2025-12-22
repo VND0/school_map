996a6504e11216fd533909950a0570e0469d9470
user <varlamov577123@gmail.com>
2025-12-22 21:43:33 +0300

    Made the site a PWA: - downloaded locally qr library - got rid of url_for, url_path_for and removed /static prefix - added manifest and service-worker - removed overscroll with css

---
+++ static/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'static-cache';
+
+const CACHED_URLS = [
+    '/',
+    '/styles/styles.css',
+    '/scripts/main.js',
+    '/manifest.json',
+    '/service-worker.js',
+    '/scripts/html5-qrcode.min.js',
+    '/img/icon-192.webp'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(CACHED_URLS);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    const url = new URL(event.request.url);
+
+    if (CACHED_URLS.includes(url.pathname)) {
+        event.respondWith(caches.match(event.request));
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return Promise.reject(new TypeError('Failed to fetch'));
+        })
+    );
+});
